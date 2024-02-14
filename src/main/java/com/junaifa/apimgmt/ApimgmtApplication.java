49f2a15c792fb38d5c466e290bc9546f66939d06
Junaid Ahmed <junaidaj86@gmail.com>
2024-02-14 21:53:47 +0100

    adding spring project structure

---
+++ src/main/java/com/junaifa/apimgmt/ApimgmtApplication.java
@@ -0,0 +1,13 @@
+package com.junaifa.apimgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junaifa/apimgmt/ApimgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junaifa.apimgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
